f302c4af43e31da432f789a82c399ff61140de58
robinsonhf <70717991+robinsonhf@users.noreply.github.com>
2025-11-18 20:07:54 -0300

    Update getDefaultImages.ts

--- getDefaultImages.ts
+++ getDefaultImages.ts
@@ -1,1 +1,23 @@
+export default function getDefaultImages() {
+  return {
+    content: [
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_100_Videos_Mujeres_1-p0VxF.jpg', title: 'Contenido Fitness & Wellness' },
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_7_Videos_Mujeres_2-0M6sJ.jpg', title: 'Contenido de Viajes & Lifestyle' },
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_105_Videos_Mujeres_7-ruDuS.jpg', title: 'Contenido Fashion & Trends' },
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_22_gempages_570357700764894432-1ac3141d-3780-4ebd-a36a-185ce37058e3-2BMPC.png', title: 'Contenido Temático Premium' },
+    ],
+    examples: [
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_170_gempages_570357700764894432-37a49508-3ab3-41ec-83a7-0c6367e8aedb-6iXFkZd0CsKE3wklTZxDkg2CABYJZr.png', title: 'Contenido de Lifestyle' },
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_167_gempages_570357700764894432-4f333a86-247c-4aa8-a38a-7c800406af41-oCLUT74ZNmzAOESq78e6UQyerqAev6.png', title: 'Contenido de Moda & Viajes' },
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_169_gempages_570357700764894432-c29f76b4-3ae2-4413-a00d-69c9707dbcb0-pRXYLirC9884UrpX4Ra3WIPXtkaMds.png', title: 'Contenido Exclusivo Premium' },
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_173_gempages_570357700764894432-1ac3141d-3780-4ebd-a36a-185ce37058e3-qOvBWmiH0WdVndZRocx83LNOqAlfvc.png', title: 'Contenido Temático Variado' },
+    ],
+    lifestyle: [
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_225_gempages_570357700764894432-819b0f0c-1fd2-494a-b43d-52d150c304d3-rq38UYkqaM6eBrmIEKOhMMb0fvBWOV.jpg' },
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_234_gempages_570357700764894432-cfb8f8a0-df62-4dd0-b9ba-6a50991418f4-T6GpI59T0zAHSNyPKrRFhJLgJlFvsX.jpg' },
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_233_gempages_570357700764894432-9c558067-c254-41bc-8a6f-0519999ce991-NDmPPplX8dI8PfPfv0lRXcdL7MI2dV.jpg' },
+      { img: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/imgi_100_Videos_Mujeres_3-LuKLEkSeFktPHOkkD77xPVOnMzurCj.jpg' },
+    ],
+  }
+}
 
